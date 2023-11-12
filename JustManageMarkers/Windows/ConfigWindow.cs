840b9a55f6d4b5a7cd664f786832c96b88d68ffa
Ethan Henderson <ethan@zbee.codes>
2023-11-12 06:49:25 -0700

    Finish moving, add .idea back

--- JustManageMarkers/Windows/ConfigWindow.cs
+++ JustManageMarkers/Windows/ConfigWindow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Numerics;
 using Dalamud.Interface.Windowing;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
 using ImGuiNET;
 
 namespace JustManageMarkers.Windows;
@@ -41,15 +42,6 @@ public class ConfigWindow : Window, IDisposable
 
     public override void Draw()
     {
-        // can't ref a property, so use a local copy
-        var configValue = this.Configuration.SomePropertyToBeSavedAndWithADefault;
-        if (ImGui.Checkbox("Random Config Bool", ref configValue))
-        {
-            this.Configuration.SomePropertyToBeSavedAndWithADefault = configValue;
-            // can save immediately on change, if you don't want to provide a "Save and Close" button
-            this.Configuration.Save();
-        }
-
         if (ImGui.BeginTabBar("MarkerModTabs"))
         {
             if (ImGui.BeginTabItem("Modifications"))
