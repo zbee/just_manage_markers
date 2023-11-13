ca45db617ba92c2b872a8b015826153f8b5632b0
Ethan Henderson <ethan@zbee.codes>
2023-11-12 17:08:09 -0700

    Configure command structures to take handlers

--- JustManageMarkers/Commands/Commands.cs
+++ JustManageMarkers/Commands/Commands.cs
@@ -1,47 +1,78 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace JustManageMarkers.Commands;
 
 public class Commands
 {
-    private List<Command> commands = new();
+    #region Boilerplate
+
+    private readonly List<Command> _commands = new();
+
+    private static void UNIMPLEMENTED_COMMAND(
+        JustManageMarkers plugin,
+        ArgumentStruct args = default
+    )
+    {
+        throw new NotImplementedException();
+    }
+
+    private static ArgumentStruct UNIMPLEMENTED_COMMAND_PARSER(
+        JustManageMarkers plugin,
+        List<List<string>> acceptedArguments,
+        string args
+    )
+    {
+        throw new NotImplementedException();
+    }
 
     public List<Command> getCommands()
+
+        #endregion
+
     {
         this._addBaseCommands();
         this._addSwapCommand();
 
-        return this.commands;
+        #region Boilerplate
+
+        return this._commands;
+
+        #endregion
     }
 
     private void _addBaseCommands()
     {
-        this.commands.Add(
+        this._commands.Add(
             new Command(
                 "/justmarkers",
+                CommandHandlers.justManageMarkers,
                 "Open the main window"
             )
         );
 
-        this.commands.Add(
+        this._commands.Add(
             new Command(
                 "/justmarkers config",
+                CommandHandlers.justManageMarkersConfig,
                 "Open the preferences window"
             )
         );
 
-        this.commands.Add(
+        this._commands.Add(
             new Command(
                 "/justmarkers help",
+                UNIMPLEMENTED_COMMAND,
                 "Open the help window",
                 true,
                 true
             )
         );
 
-        this.commands.Add(
+        this._commands.Add(
             new Command(
                 "/justmarkers advanced help",
+                UNIMPLEMENTED_COMMAND,
                 "Open the advanced help window"
             )
         );
@@ -49,31 +80,38 @@ public class Commands
 
     private void _addSwapCommand()
     {
-        this.commands.Add(
+        this._commands.Add(
             new Command(
                 "/justmarkers swap",
+                UNIMPLEMENTED_COMMAND,
                 "Swap the positions of two markers, or marker types",
-                new List<List<string>>() {
-                    new List<string>() {
+                new List<List<string>>()
+                {
+                    new List<string>()
+                    {
                         "letters",
                         "numbers",
                     },
-                    new List<string>() {
+                    new List<string>()
+                    {
                         "numbers",
                         "letters",
                     },
-                    new List<string>() {
+                    new List<string>()
+                    {
                         "<marker>",
                         "<marker>",
                     },
                 },
+                UNIMPLEMENTED_COMMAND_PARSER,
                 false
             )
         );
 
-        this.commands.Add(
+        this._commands.Add(
             new Command(
                 "/justmarkers swap help",
+                UNIMPLEMENTED_COMMAND,
                 "Learn how to us the swap command",
                 false
             )
--- JustManageMarkers/Commands/Struct.cs
+++ JustManageMarkers/Commands/Struct.cs
@@ -5,32 +5,52 @@ namespace JustManageMarkers.Commands;
 public struct Command
 {
     public string Name { get; }
+    public HandlerDelegate Handler { get; }
     public string ShortDescription { get; }
     public List<List<string>>? Arguments { get; } // If a string ends with ?, it is optional
+    public ArgumentParserDelegate? ArgumentParser { get; }
     public string? Description { get; }
     public bool SpaceAbove { get; } = false;
     public bool IncludeInHelp { get; } = true;
+
+
+    public delegate void HandlerDelegate(JustManageMarkers plugin, ArgumentStruct args = default);
+
+    public delegate ArgumentStruct ArgumentParserDelegate(JustManageMarkers plugin, List<List<string>> acceptedArguments, string args);
+
 
     #region Constructors
 
     #region No arguments, no description
 
-    public Command(string name, string shortDescription) : this()
+    public Command(string name, HandlerDelegate handler, string shortDescription) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
     }
 
-    public Command(string name, string shortDescription, bool includeInHelp) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        bool includeInHelp
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.IncludeInHelp = includeInHelp;
     }
 
-    public Command(string name, string shortDescription, bool spaceAbove, bool includeInHelp) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        bool spaceAbove,
+        bool includeInHelp
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.SpaceAbove = spaceAbove;
         this.IncludeInHelp = includeInHelp;
@@ -40,24 +60,42 @@ public struct Command
 
     #region No arguments, with description
 
-    public Command(string name, string shortDescription, string description) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        string description
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.Description = description;
     }
 
-    public Command(string name, string shortDescription, string description, bool includeInHelp) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        string description,
+        bool includeInHelp
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.IncludeInHelp = includeInHelp;
         this.Description = description;
     }
 
-    public Command(string name, string shortDescription, string description, bool spaceAbove, bool includeInHelp) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        string description,
+        bool spaceAbove,
+        bool includeInHelp
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.SpaceAbove = spaceAbove;
         this.IncludeInHelp = includeInHelp;
@@ -66,31 +104,58 @@ public struct Command
 
     #endregion
 
-    #region Argments, no description
+    #region Arguments, no description
 
-    public Command(string name, string shortDescription, string description, List<List<string>> arguments) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        string description,
+        List<List<string>> arguments,
+        ArgumentParserDelegate argumentParser
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.Description = description;
         this.Arguments = arguments;
+        this.ArgumentParser = argumentParser;
     }
 
-    public Command(string name, string shortDescription, string description, List<List<string>> arguments, bool includeInHelp) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        string description,
+        List<List<string>> arguments,
+        ArgumentParserDelegate argumentParser,
+        bool includeInHelp
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.Description = description;
         this.Arguments = arguments;
+        this.ArgumentParser = argumentParser;
         this.IncludeInHelp = includeInHelp;
     }
 
-    public Command(string name, string shortDescription, string description, List<List<string>> arguments, bool spaceAbove, bool includeInHelp) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        string description,
+        List<List<string>> arguments,
+        ArgumentParserDelegate argumentParser,
+        bool spaceAbove,
+        bool includeInHelp
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.Description = description;
         this.Arguments = arguments;
+        this.ArgumentParser = argumentParser;
         this.SpaceAbove = spaceAbove;
         this.IncludeInHelp = includeInHelp;
     }
@@ -99,26 +164,50 @@ public struct Command
 
     #region Arguments, with description
 
-    public Command(string name, string shortDescription, List<List<string>> arguments) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        List<List<string>> arguments,
+        ArgumentParserDelegate argumentParser
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.Arguments = arguments;
+        this.ArgumentParser = argumentParser;
     }
 
-    public Command(string name, string shortDescription, List<List<string>> arguments, bool includeInHelp) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        List<List<string>> arguments,
+        ArgumentParserDelegate argumentParser,
+        bool includeInHelp
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.Arguments = arguments;
+        this.ArgumentParser = argumentParser;
         this.IncludeInHelp = includeInHelp;
     }
 
-    public Command(string name, string shortDescription, List<List<string>> arguments, bool spaceAbove, bool includeInHelp) : this()
+    public Command(string name,
+        HandlerDelegate handler,
+        string shortDescription,
+        List<List<string>> arguments,
+        ArgumentParserDelegate argumentParser,
+        bool spaceAbove,
+        bool includeInHelp
+    ) : this()
     {
         this.Name = name;
+        this.Handler = handler;
         this.ShortDescription = shortDescription;
         this.Arguments = arguments;
+        this.ArgumentParser = argumentParser;
         this.SpaceAbove = spaceAbove;
         this.IncludeInHelp = includeInHelp;
     }
