36fc8a1d8f13462f7d10299b2195d0af4cdabf1b
Ethan Henderson <ethan@zbee.codes>
2023-11-14 00:13:21 -0700

    Include documentation that will be needed

--- JustManageMarkers/Core/WaymarkWrapper.cs
+++ JustManageMarkers/Core/WaymarkWrapper.cs
@@ -2,5 +2,9 @@
 
 public class WaymarkWrapper
 {
-    
+    // we can check WaymarkPreset's IPCs like this: https://l.zbee.me/40JlipB
+    // (with WaymarkPreset's IPCs of course: https://l.zbee.me/40DWeQS)
+    // (IPC doc https://l.zbee.me/3QXRv9x)
+    // we can check WaymarkPreset's callable like this: https://l.zbee.me/40ysULH
+    // invoke commands like this: https://l.zbee.me/49zmVKw
 }
