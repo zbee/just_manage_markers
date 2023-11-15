1d1de1000844cb388705e2932e27e5c624264b25
Ethan Henderson <ethan@zbee.codes>
2023-11-15 11:52:20 -0700

    Adjust swapping of letters and numbers to happen all at once

--- JustManageMarkers/Functions/Swap.cs
+++ JustManageMarkers/Functions/Swap.cs
@@ -12,18 +12,77 @@ public class Swap
     {
         JustManageMarkers.Log.Debug("Swapping Types");
 
+        // Fill a preset with current game waymarks
+        var gamePreset = JustManageMarkers.WaymarkPresetAPI.createEmptyGamePreset();
+        if (!JustManageMarkers.WaymarkPresetAPI.getCurrentWaymarksAsPreset(ref gamePreset))
+        {
+            JustManageMarkers.Log.Error(
+                "WaymarkPresetPlugin Failed to get current waymarks"
+            );
+
+            JustManageMarkers.Chat.Print(
+                "Could not get current waymarks. Do you have any placed?"
+            );
+
+            return;
+        }
+
+        JustManageMarkers.Log.Verbose(
+            "Current Waymarks: "
+            + JsonConvert.SerializeObject(
+                gamePreset
+            )
+        );
+
+        // Build an empty list of waymarks
+        var blank = JustManageMarkers.WaymarkPresetAPI.createEmptyGamePresetPoint();
+        var modifiedWaymarks = new List<object>()
+        {
+            blank,
+            blank,
+            blank,
+            blank,
+            blank,
+            blank,
+            blank,
+            blank,
+        };
+
         var marksByColor = Markers.markers.GroupBy(
             mark => mark.Color
         );
 
         foreach (var colors in marksByColor.Select(colorGroup => colorGroup.ToList()))
         {
-            swapMarks(
-                colors[0],
-                colors[1],
-                true
-            );
+            // Get the currently applicable waymarks from the game's waymarks
+            var waymarkOne = gamePreset.GetType()
+                .GetField(colors[0].Name)!.GetValue(gamePreset)!;
+
+            var waymarkTwo = gamePreset.GetType()
+                .GetField(colors[1].Name)!.GetValue(gamePreset)!;
+
+            // Swap the values of the currently applicable waymarks
+            modifiedWaymarks[colors[1].Index] = waymarkOne;
+            modifiedWaymarks[colors[0].Index] = waymarkTwo;
         }
+
+        // Create a new WaymarkPreset with the modified waymark values over the game's current marks
+        var swappedWaymarks = JustManageMarkers.WaymarkPresetAPI
+            .modifyCurrentWaymarkPresetWithPresetPoints(
+                modifiedWaymarks
+            );
+
+        JustManageMarkers.Log.Verbose(
+            "Swapped Waymarks: "
+            + JsonConvert.SerializeObject(
+                swappedWaymarks
+            )
+        );
+
+        // Place the new waymarks
+        // TODO: if this is false, it works in duties but not the overworld
+        // If I recall correctly: if it's true, it works in the overworld but with division issues (toggleable division by 1000?)
+        JustManageMarkers.WaymarkPresetAPI.placeWaymarks(swappedWaymarks, false);
 
         JustManageMarkers.Log.Info(
             "Swapped types"
@@ -35,11 +94,7 @@ public class Swap
         );
     }
 
-    public static void swapMarks(
-        Marker markOne,
-        Marker markTwo,
-        bool quiet = false
-    )
+    public static void swapMarks(Marker markOne, Marker markTwo)
     {
         JustManageMarkers.Log.Debug(
             $"Swapping {markOne.ShortName} and {markTwo.ShortName}"
@@ -60,7 +115,7 @@ public class Swap
             return;
         }
 
-        JustManageMarkers.Log.Debug(
+        JustManageMarkers.Log.Verbose(
             "Current Waymarks: "
             + JsonConvert.SerializeObject(
                 gamePreset
@@ -98,8 +153,8 @@ public class Swap
                 modifiedWaymarks
             );
 
-        JustManageMarkers.Log.Debug(
-            "Current Waymarks: "
+        JustManageMarkers.Log.Verbose(
+            "Swapped Waymarks: "
             + JsonConvert.SerializeObject(
                 swappedWaymarks
             )
@@ -114,10 +169,9 @@ public class Swap
             $"Swapped {markOne.ShortName} and {markTwo.ShortName}"
         );
 
-        if (!quiet)
-            JustManageMarkers.Chat.Print(
-                $"Swapped {markOne.ShortName} and {markTwo.ShortName}",
-                JustManageMarkers.Name
-            );
+        JustManageMarkers.Chat.Print(
+            $"Swapped {markOne.ShortName} and {markTwo.ShortName}",
+            JustManageMarkers.Name
+        );
     }
 }
