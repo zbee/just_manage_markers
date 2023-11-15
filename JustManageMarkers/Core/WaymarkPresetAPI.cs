ff3bc1a90a40bdfb2fbb98eba9e047323a68c007
Ethan Henderson <ethan@zbee.codes>
2023-11-14 23:19:51 -0700

    Implemented technique for working with a full GamePreset from empty Implemented other endpoints needed for that technique, and swapping waymarks in general

--- JustManageMarkers/Core/WaymarkPresetAPI.cs
+++ JustManageMarkers/Core/WaymarkPresetAPI.cs
@@ -35,6 +35,10 @@ public class WaymarkPresetAPI : IDisposable
 
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+    private Type? _waymarkPreset_GamePresetPoint { get; set; }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
     private Type? _waymarkPreset_MemoryHandler { get; set; }
 
     [SuppressMessage("ReSharper", "InconsistentNaming")]
@@ -205,11 +209,33 @@ public class WaymarkPresetAPI : IDisposable
         )!;
     }
 
+    public object createEmptyGamePresetPoint()
+    {
+        this._checkConnection();
+
+        // Return a new instance of the GamePresetPoint class
+        return this._waymarkPreset_GamePresetPoint!.Assembly.CreateInstance(
+            this._waymarkPreset_GamePresetPoint.FullName!
+        )!;
+    }
+
+    public string gamePresetPointToString(object gamePresetPoint)
+    {
+        this._checkConnection();
+
+        // Run the ToString method on the GamePresetPoint
+        return (gamePresetPoint.GetType().GetMethod("ToString")!
+            .Invoke(
+                gamePresetPoint,
+                Array.Empty<object>()
+            ) as string)!;
+    }
+
     public object createEmptyWaymarkPreset()
     {
         this._checkConnection();
 
-        // Return a new instance of the GamePreset class
+        // Return a new instance of the WaymarkPreset class
         return this._waymarkPreset_WaymarkPreset!.Assembly.CreateInstance(
             this._waymarkPreset_WaymarkPreset.FullName!
         )!;
@@ -219,19 +245,24 @@ public class WaymarkPresetAPI : IDisposable
     {
         this._checkConnection();
 
-        throw new NotImplementedException();
+        // Call the method, converting the passed preset
+        return gamePreset.GetType().GetMethod("GetAsGamePreset")!
+            .Invoke(
+                gamePreset,
+                Array.Empty<object>()
+            )!;
     }
 
     public bool getCurrentWaymarksAsPreset(ref object preset)
     {
         this._checkConnection();
 
-        var argument = new object[]
+        var argument = new[]
         {
             preset
         };
 
-        // Call te method
+        // Call the method
         var result = this._waymarkPreset_MemoryHandler!
             .GetMethod("GetCurrentWaymarksAsPresetData")!.Invoke(
                 this._waymarkPreset_MemoryHandler!.FullName,
@@ -242,6 +273,116 @@ public class WaymarkPresetAPI : IDisposable
         preset = argument[0];
 
         return result != null;
+    }
+
+    public void placeWaymarks(object gamePreset, bool allowClientSide = false)
+    {
+        this._checkConnection();
+
+        // Pass the preset and the client-side preference
+        var argument = new[]
+        {
+            gamePreset,
+            allowClientSide
+        };
+
+        // Call the method
+        this._waymarkPreset_MemoryHandler!
+            .GetMethod("PlacePreset")!.Invoke(
+                this._waymarkPreset_MemoryHandler!.FullName,
+                argument
+            );
+    }
+
+    public object modifyCurrentWaymarkPresetWithPresetPoints(List<object> modifiedGamePreset)
+    {
+        this._checkConnection();
+
+        // Create an empty preset to work from
+        var newWaymarksPreset = this.createEmptyWaymarkPreset();
+
+        // Get the current waymarks
+        var currentGamePreset = this.createEmptyGamePreset();
+        this.getCurrentWaymarksAsPreset(ref currentGamePreset);
+
+        // Make an empty GamePresetPoint to compare against
+        var emptyGamePresetPoint = this.createEmptyGamePresetPoint();
+        var blank = this.gamePresetPointToString(emptyGamePresetPoint);
+
+        var waymarkPresetAttributes = new[]
+        {
+            "X",
+            "Y",
+            "Z",
+            "Active"
+        };
+
+        // Loop through each waymark in the preset and update its attributes
+        foreach (var marker in Markers.markers)
+        {
+            // Set current waymark in the new WaymarkPreset to edit
+            var newMark
+                = newWaymarksPreset.GetType().GetProperty(marker.Name)!
+                    .GetValue(newWaymarksPreset)!;
+
+            // Set current waymark in the modified GamePresetPoints to fill from
+            var modifiedMark = modifiedGamePreset[marker.Index];
+
+            // Set current waymark in the current WaymarkPreset to fill from
+            var currentMark
+                = currentGamePreset.GetType().GetField(marker.Name)!.GetValue(currentGamePreset)!;
+
+            // Get the string value of the modified and current waymarks to check if blank
+            var testIfModifiedBlank = this.gamePresetPointToString(
+                modifiedGamePreset[marker.Index]
+            );
+
+            var testIfCurrentBlank = this.gamePresetPointToString(
+                currentMark
+            );
+
+            // Loop through the available attributes for the new WaymarkPreset
+            foreach (var attribute in waymarkPresetAttributes)
+            {
+                // Set up the attribute that will be changed in the new WaymarkPreset
+                var changingAtt = newMark.GetType().GetProperty(attribute)!;
+
+                // Short circuit assign active attribute
+                if (attribute == "Active")
+                {
+                    var active = testIfModifiedBlank != blank
+                                 || testIfCurrentBlank != blank;
+
+                    changingAtt.SetValue(
+                        newMark,
+                        active
+                    );
+
+                    continue;
+                }
+
+                // Fill with current waymark position float
+                var fillingAtt = (int) currentMark.GetType().GetField(attribute)!
+                                     .GetValue(currentMark)!
+                                 / 1000.0f;
+
+                // Instead fill with the modified position float if it is not blank
+                if (testIfModifiedBlank != blank)
+                {
+                    // Get the equivalent attribute from the swapped GamePresetPoint
+                    fillingAtt = (int) modifiedMark.GetType().GetField(attribute)!
+                                     .GetValue(modifiedMark)!
+                                 / 1000.0f;
+                }
+
+                changingAtt.SetValue(
+                    newMark,
+                    fillingAtt
+                );
+            }
+        }
+
+        return this.getWaymarkPresetAsGamePreset(newWaymarksPreset);
     }
 
     # endregion
